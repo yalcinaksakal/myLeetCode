2af0e30f85d71af5a35caafbad6268787351bfec
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-09-05 16:36:45 +0200

    resizer done

--- components/Problem/Problem.tsx
+++ components/Problem/Problem.tsx
@@ -9,7 +9,24 @@ const Problem: React.FC = () => {
 
 Aliqua aliquip ullamco magna magna exercitation minim Lorem esse exercitation velit. Occaecat duis voluptate elit proident laboris eu pariatur exercitation eu nisi duis ipsum. Labore pariatur laboris non enim culpa. Consequat aute deserunt laborum sint dolore tempor est tempor.
 
-Proident voluptate anim nulla ad elit aliqua. Deserunt mollit ipsum occaecat id. Non commodo in excepteur et irure nulla esse adipisicing aliquip laborum laborum velit nisi. Voluptate sint Lorem minim Lorem."
+Proident voluptate anim nulla ad elit aliqua. Deserunt mollit ipsum occaecat id. Non commodo in excepteur et irure nulla esse adipisicing aliquip laborum laborum velit nisi. Voluptate sint Lorem minim Lorem.
+In mollit adipisicing Lorem do reprehenderit voluptate aliqua esse amet. Minim ea magna elit in nostrud Lorem sunt ad reprehenderit amet exercitation. Cillum anim mollit id quis velit dolore ex Lorem do nostrud. Ex ullamco occaecat nisi incididunt velit eiusmod excepteur sint quis culpa.
+
+Aliqua aliquip ullamco magna magna exercitation minim Lorem esse exercitation velit. Occaecat duis voluptate elit proident laboris eu pariatur exercitation eu nisi duis ipsum. Labore pariatur laboris non enim culpa. Consequat aute deserunt laborum sint dolore tempor est tempor.
+
+Proident voluptate anim nulla ad elit aliqua. Deserunt mollit ipsum occaecat id. Non commodo in excepteur et irure nulla esse adipisicing aliquip laborum laborum velit nisi. Voluptate sint Lorem minim Lorem.
+In mollit adipisicing Lorem do reprehenderit voluptate aliqua esse amet. Minim ea magna elit in nostrud Lorem sunt ad reprehenderit amet exercitation. Cillum anim mollit id quis velit dolore ex Lorem do nostrud. Ex ullamco occaecat nisi incididunt velit eiusmod excepteur sint quis culpa.
+
+Aliqua aliquip ullamco magna magna exercitation minim Lorem esse exercitation velit. Occaecat duis voluptate elit proident laboris eu pariatur exercitation eu nisi duis ipsum. Labore pariatur laboris non enim culpa. Consequat aute deserunt laborum sint dolore tempor est tempor.
+
+Proident voluptate anim nulla ad elit aliqua. Deserunt mollit ipsum occaecat id. Non commodo in excepteur et irure nulla esse adipisicing aliquip laborum laborum velit nisi. Voluptate sint Lorem minim Lorem.
+In mollit adipisicing Lorem do reprehenderit voluptate aliqua esse amet. Minim ea magna elit in nostrud Lorem sunt ad reprehenderit amet exercitation. Cillum anim mollit id quis velit dolore ex Lorem do nostrud. Ex ullamco occaecat nisi incididunt velit eiusmod excepteur sint quis culpa.
+
+Aliqua aliquip ullamco magna magna exercitation minim Lorem esse exercitation velit. Occaecat duis voluptate elit proident laboris eu pariatur exercitation eu nisi duis ipsum. Labore pariatur laboris non enim culpa. Consequat aute deserunt laborum sint dolore tempor est tempor.
+
+Proident voluptate anim nulla ad elit aliqua. Deserunt mollit ipsum occaecat id. Non commodo in excepteur et irure nulla esse adipisicing aliquip laborum laborum velit nisi. Voluptate sint Lorem minim Lorem.
+
+"
     ></textarea>
   );
 };
--- pages/editor/index.tsx
+++ pages/editor/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from "next";
 import dynamic from "next/dynamic";
+import { useEffect, useRef, useState } from "react";
 import Resizer from "../../components/DragableLine/DragableLine";
 import Problem from "../../components/Problem/Problem";
 import styles from "./editor.module.scss";
@@ -7,11 +8,50 @@ import styles from "./editor.module.scss";
 const CodeEditor = dynamic(import("../../components/Ace/Ace"), { ssr: false });
 
 const Editor: NextPage = () => {
+  const [widths, setWidths] = useState({ prblm: "40%", edtr: "40%" });
+  const pageWidth = useRef(5000);
+  const isResizing = useRef(false);
+  useEffect(() => {
+    pageWidth.current = window.innerWidth;
+    const width = Math.floor(pageWidth.current / 2) - 5 + "px";
+    setWidths({ prblm: width, edtr: width });
+  }, []);
+
+  const resize = pageX => {
+    console.log(pageX);
+    if (pageX < 150 || pageX > pageWidth.current - 150) {
+      pageX = pageX < 150 ? 180 : pageWidth.current;
+      pageX = pageX > pageWidth.current - 150 ? pageWidth.current - 180 : pageX;
+      isResizing.current = false;
+    }
+    const edtr = `${pageWidth.current - pageX - (pageX ? 20 : 0)}px`;
+    const prblm = `${pageX - 5}px`;
+    setWidths({
+      prblm,
+      edtr,
+    });
+  };
   return (
-    <div className={styles.editor}>
-      <Problem />
-      <Resizer />
-      <CodeEditor />
+    <div
+      className={styles.editor}
+      onMouseMove={e => {
+        if (isResizing.current) resize(e.pageX);
+      }}
+      // onMouseUp={() => (isResizing.current = false)}
+    >
+      <div style={{ width: widths.prblm, height: "100%" }}>
+        <Problem />
+      </div>
+      <div
+        onMouseDown={() => (isResizing.current = true)}
+        onMouseUp={() => (isResizing.current = false)}
+        style={{ height: "100%" }}
+      >
+        <Resizer />
+      </div>
+      <div style={{ width: widths.edtr, height: "100%" }}>
+        <CodeEditor />
+      </div>
     </div>
   );
 };
