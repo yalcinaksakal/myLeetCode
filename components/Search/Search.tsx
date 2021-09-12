f33ee27152cb363b68edf9babc513358c02dbe03
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-09-12 16:35:25 +0200

    search component's styling ok

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,6 +1,31 @@
 import styles from "./Search.module.scss";
-
+import { useState } from "react";
 const Search = () => {
-  return <input className={styles.search} type="search" placeholder="Search" />;
+  const [showResults, setShowResults] = useState(false);
+  const [filter, setFilter] = useState("");
+  return (
+    <div className={styles.search}>
+      <input
+        className={styles.box}
+        type="search"
+        placeholder="Search"
+        onMouseEnter={() => setShowResults(true)}
+        onChange={e => {
+          const text = e.currentTarget.value;
+          const newFilter = text.trim();
+          if (newFilter !== filter) setFilter(newFilter);
+        }}
+      />
+      {showResults && filter.length > 0 && (
+        <div
+          className={styles.result}
+          onMouseEnter={() => setShowResults(true)}
+          onMouseLeave={() => setShowResults(false)}
+        >
+          Search Results
+        </div>
+      )}
+    </div>
+  );
 };
 export default Search;
